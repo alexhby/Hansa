ddd9269adcba6ec21bb057b530afd384e5707c94
jerrynomenon <jerrynomenon@live.com>
2016-04-01 12:37:08 -0400

    Updated menu 2016-04-01

--- Jeremy/Assets/Scripts/EscMenu.cs
+++ Jeremy/Assets/Scripts/EscMenu.cs
@@ -1,34 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+/*
+    Class that controls tab buttons in pause menu
+*/
 public class EscMenu : MonoBehaviour
 {
 
-    public GameObject pauseMenuPanel;
+    public GameObject pauseMenuPanel;                   // reference to pause menu panel
 
     private bool isPaused;                              //Boolean to check if the game is paused or not
-    private StartOptions startScript;                   //Reference to the StartButton script
 
-    // reference to four menu panels
-    public GameObject charactersPanel;
+    // reference to menu panels
+    public GameObject charactersPanel;                  
     public GameObject inventoryPanel;
     public GameObject questPanel;
     public GameObject gamePanel;
+    public GameObject charDetailPanel;
+    public GameObject questDetailPanel;
+    public GameObject inventoryDetailPanel;
 
+    // reference to main camera
     public GameObject mainCamera;
-    private NewBehaviourScript blurOptimized;
 
     //Awake is called before Start()
     void Awake()
     {
+
     }
 
     void Start()
     {
         isPaused = false;
-        blurOptimized = (NewBehaviourScript)mainCamera.GetComponent("BlurOptimized");
     }
-    // Update is called once per frame
+    // Press Esc to pause/unpause
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -43,6 +47,8 @@ public class EscMenu : MonoBehaviour
         }
     }
 
+    // function to control tab buttons' behaivour - pass parameter within OnClick() within each button
+    // 1 = characters, 2 = inventory, 3 = quest, 4 = game
     public void tabButtonPressed(int panelToGo)
     {
         // first hide all panels
@@ -50,8 +56,11 @@ public class EscMenu : MonoBehaviour
         inventoryPanel.SetActive(false);
         questPanel.SetActive(false);
         gamePanel.SetActive(false);
+        charDetailPanel.SetActive(false);
+        questDetailPanel.SetActive(false);
+        inventoryDetailPanel.SetActive(false);
 
-        switch(panelToGo)
+        switch (panelToGo) // show panel according to panelToGo parameter
         {
             case 1: charactersPanel.SetActive(true); break;
             case 2: inventoryPanel.SetActive(true); break;
